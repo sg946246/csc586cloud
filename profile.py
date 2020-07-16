ad7e846e7b1ffc59cf01d272a0966b3ad3230113
sg946246 <68036597+sg946246@users.noreply.github.com>
2020-07-15 20:17:41 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -9,13 +9,13 @@ node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
-node.addService(rspec.EXecute(Shell="/bin/sh",
+node.addService(rspec.EXecute(shell="/bin/sh",
                               command="sudo apt update"))
-node.addService(rspec.EXecute(Shell="/bin/sh",
+node.addService(rspec.EXecute(shell="/bin/sh",
                               command="sudo apt install -y apache2"))
-node.addService(rspec.EXecute(Shell="/bin/sh",
+node.addService(rspec.EXecute(shell="/bin/sh",
                               command='sudo uwf allow in "Apache Full"'))
-node.addService(rspec.EXecute(Shell="/bin/sh",
+node.addService(rspec.EXecute(shell="/bin/sh",
                               command='sudo systemctl status apache2'))
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRspec()
