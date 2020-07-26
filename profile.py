b199958a30d8d2f3e35041a15e282ec435db089c
sg946246 <68036597+sg946246@users.noreply.github.com>
2020-07-26 19:35:45 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -27,27 +27,17 @@ for i in range(2):
     node.addService(rspec.Execute(shell="/bin/sh",
                               command='sudo systemctl status apache2'))
     node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo mkdir -p /var/webserver_log'))
-    
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-    iface = node.addInterface("if" + str(i))
-    iface.component_id = "eth1"
-    iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-    link.addInterface(iface)
-    
+                              command='sudo mkdir -p /var/webserver_log'))    
   else:
     node = request.XenVM("observer")
     node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt update"))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt-get install nfs-kernel-server"))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo apt-get install nfs-common"'))
+                              command="sudo apt-get install -y nfs-kernel-server"))
     node.addService(rspec.Execute(shell="/bin/sh",
                               command='sudo mkdir -p /var/webserver_monitor'))
     node.addService(rspec.Execute(shell="/bin/sh",
                               command='sudo chown nobody:nogroup /var/webserver_monitor'))
    
+  
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
     iface = node.addInterface("if" + str(i))
     iface.component_id = "eth1"
